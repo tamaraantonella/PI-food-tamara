f4c920ba75af4a35e954a49e5419d631f5cc0f36
Tamara Frazzetta <tamarafrazzetta@gmail.com>
2022-08-25 22:49:39 -0300

    recipeDetail styled

--- client/src/components/RecipeDetail/RecipeDetail.jsx
+++ client/src/components/RecipeDetail/RecipeDetail.jsx
@@ -4,6 +4,7 @@ import React, {useEffect} from 'react'
 import { getDetail, resetDetail } from '../../actions/index'
 import { useParams } from 'react-router-dom'
 import { Link } from 'react-router-dom'
+import s from './recipeDetail.module.css'
 
 export default function RecipeDetail(props) {
   let {id} = useParams()
@@ -19,20 +20,28 @@ export default function RecipeDetail(props) {
   },[dispatch,id])
   
   return (
-    <div>
-      <Link to='/home'><button>Go back</button></Link>
+    <div className={s.detailContainer}> 
       {detail[0]?
-        
-          <div>
-              <h1>{detail[0].name}</h1>
-              <p>HealthScore: {detail[0].healthScore}</p>
+          <div className={s.detailBox}>
+            <div className={s.detailHeader}>
+              <Link to='/home'><button className={s.detailButton}>Go back</button></Link>
+              <h1 className={s.detailHeader_title}>{detail[0].name}</h1>
+            </div>
+            <p className={s.detailHealth}>HealthScore: {detail[0].healthScore}</p>
+            <div className={s.detailMiddle}>
               <img src={detail[0].image} alt="" />
+              <div className={s.detailSteps} >
+                <p>Steps</p>
+                <p>{detail[0].steps}</p>
+              </div>
+            </div>
+            <div className={s.detailSummary}>
+
               <p>Summary:</p>
               <div
                   dangerouslySetInnerHTML={{__html: detail[0].summary}}
                 />
-              <p>Steps</p>
-              <p>{detail[0].steps}</p>
+            </div>
           </div>
           
           :
