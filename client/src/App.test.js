436b3aa5bfb4b51362c09afed6a635bb2da16d62
Tamara Frazzetta <tamarafrazzetta@gmail.com>
2022-08-27 15:50:29 -0300

    navbar test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,25 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+import {BrowserRouter} from 'react-router-dom'
+import Navbar from './components/Navbar/Navbar';
+import {Provider} from 'react-redux';
+import store from './store/index';
+
+test('renders the name of the app', () => {
+  render(<Provider store={store}>
+    <BrowserRouter>
+      <Navbar/>
+      </BrowserRouter>
+    </Provider>);
+  const linkElement = screen.getByText('Foody App');
   expect(linkElement).toBeInTheDocument();
+});
+
+test('renders a Create recipe', () => {
+  render(<Provider store={store}>
+    <BrowserRouter><Navbar/></BrowserRouter>
+    
+</Provider>);
+  const create = screen.getByText('Create recipe');
+  expect(create).toBeInTheDocument();
 });
